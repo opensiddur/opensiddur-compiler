c13acfbce4fbf125b7d92a27c8c813d15eec9208
Efraim Feinstein <efraim.feinstein@gmail.com>
2019-11-10 21:13:44 -0800

    test for SelectableResultsList

---
+++ src/SearchableSelectableResultsList.test.js
@@ -0,0 +1,5 @@
+/* SearchableSelectableResultsList.test.js
+ * Copyright 2019 Efraim Feinstein <efraim@opensiddur.org>
+ * Open Siddur Project
+ * Licensed under the GNU Lesser General Public License, version 3 or later
+ */
